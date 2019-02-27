f9738d732851278d20dddb8409c8fcf87f919114
卑微小魏 <1223850908@qq.com>
2019-02-27 09:45:17 +0800

    添加lucene工程至git

---
+++ src/test/java/testCreateIndex.java
@@ -0,0 +1,15 @@
+import org.apache.lucene.store.FSDirectory;
+
+
+
+public class testCreateIndex {
+    public static void main(String[] args) {
+        //指定索引库存的路径
+        String indexDirectory = "D:\\temp\\index";
+        //打开目录
+
+    }
+
+
+
+}
